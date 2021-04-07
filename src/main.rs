ebfd28536cdbb0ae9fd7dd5559ae1d7090da8810
gregoryquick <gregory@quicks.me>
2021-04-07 14:56:06 -0400

    Implement forward pass
    Currently broken: problem with buffer sizes?

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,35 @@ use rand::prelude::*;
 use futures::executor::block_on;
 
 fn main() {
+    //Network parameters
     const DATA_DIM: usize = 16;
     const OUTPUT_DIM: usize = 2;
-    let mut rng = rand::thread_rng();
+
+    //Make gpu pipelines
     let mut gpu_pipeline = block_on(PipelineManager::new(DATA_DIM, OUTPUT_DIM));
     let forward_pipeline = gpu_pipeline.new_pipeline::<ForwardPass, f64>();
-    let random_data = create_random_array::<DATA_DIM>(&mut rng);
-    println!("{:?}", random_data);
-}
-
-//How to make this generic over datatype?
-fn create_random_array<const SIZE: usize>(rng: &mut rand::rngs::ThreadRng) -> [f64; SIZE] {
-    let mut vector: [f64; SIZE] = [0f64; SIZE];
-    for num in vector.iter_mut() {
-        *num = rng.gen();
-    }
-    vector
+    
+    //Make network weights and a test input
+    let mut rng = rand::thread_rng();
+    let network_weights = {
+        const WEIGHT_SIZE: usize = DATA_DIM * OUTPUT_DIM;
+        let mut vector: [f64; WEIGHT_SIZE] = [0f64; WEIGHT_SIZE];
+        for num in vector.iter_mut() {
+            *num = rng.gen();
+        }
+        vector
+    };
+    let input_vector = {
+        let mut vector: [f64; DATA_DIM] = [0f64; DATA_DIM];
+        for num in vector.iter_mut() {
+            *num = rng.gen();
+        }
+        vector
+    };
+    
+    //Compute forward pass result
+    let result = block_on(gpu_pipeline.run_forward_pass::<f64>(forward_pipeline, &network_weights, &input_vector)).unwrap();
+    println!("{:?}", result);
 }
 
 struct PipelineManager {
@@ -26,7 +39,6 @@ struct PipelineManager {
     device: wgpu::Device,
     queue: wgpu::Queue,
     network_shape: (usize, usize),
-    //network_buffer: wgpu::Buffer,
 }
 
 impl PipelineManager{
@@ -58,6 +70,109 @@ impl PipelineManager{
 
     fn new_pipeline<P: Pipeline, T: bytemuck::Pod>(&self) -> P {
         P::new::<T>(&self.device, self.network_shape.0, self.network_shape.1)
+    }
+
+    async fn run_forward_pass<T: bytemuck::Pod>(&self, pipeline: ForwardPass, network_weights: &[T], input_vector: &[T]) -> Option<Vec<T>> {
+        let type_size = std::mem::size_of::<T>() as wgpu::BufferAddress;
+
+        //Create command encoder
+        let mut encoder = self.device.create_command_encoder(
+            &wgpu::CommandEncoderDescriptor {
+                label: None 
+            }
+        );
+
+        //Load data into gpu
+        use wgpu::util::{BufferInitDescriptor, DeviceExt};
+
+        let buffers = pipeline.get_buffers();
+
+        let weight_data_buffer = self.device.create_buffer_init(
+            &BufferInitDescriptor {
+                label: None,
+                contents: bytemuck::cast_slice(network_weights),
+                usage: wgpu::BufferUsage::COPY_SRC,
+            }
+        );
+        encoder.copy_buffer_to_buffer(
+            &weight_data_buffer, 0,
+            &buffers[0], 0,
+            (type_size * self.network_shape.0 as u64 * self.network_shape.1 as u64) as wgpu::BufferAddress,
+        );
+
+        let input_data_buffer = self.device.create_buffer_init(
+            &BufferInitDescriptor {
+                label: None,
+                contents: bytemuck::cast_slice(input_vector),
+                usage: wgpu::BufferUsage::COPY_SRC,
+            }
+        );
+        encoder.copy_buffer_to_buffer(
+            &weight_data_buffer, 0,
+            &buffers[1], 0,
+            (type_size * self.network_shape.0 as u64) as wgpu::BufferAddress,
+        );
+
+        //Create the compute pass (Mutably borrows encoder)
+        let mut compute_pass = encoder.begin_compute_pass(
+            &wgpu::ComputePassDescriptor { 
+                label: None 
+            }
+        );
+        
+        //Add compute pipeline to compute pass
+        compute_pass.set_pipeline(pipeline.get_compute_pipeline());
+        compute_pass.set_bind_group(0, pipeline.get_bind_group(), &[]);
+        //Work groups of x=output_size, Y = 1, Z = 1
+        compute_pass.dispatch(self.network_shape.1 as u32, 1, 1);
+        
+        //Encoder borrow is gone now!
+        drop(compute_pass);
+
+        //Copy output from gpu buffer to staging buffer on cpu
+        let staging_buffer = self.device.create_buffer(
+            &wgpu::BufferDescriptor {
+                label: Some("Staging buffer"),
+                size: (type_size * self.network_shape.1 as u64) as wgpu::BufferAddress,
+                usage: wgpu::BufferUsage::MAP_READ | wgpu::BufferUsage::COPY_DST,
+                mapped_at_creation: false,
+            }
+        );
+        encoder.copy_buffer_to_buffer(
+            &buffers[2], 0,
+            &staging_buffer, 0,
+            (type_size * self.network_shape.0 as u64) as wgpu::BufferAddress,
+        );
+
+        //Finish building command encoder and submit
+        self.queue.submit(Some(encoder.finish()));
+
+        //Create future of the computation
+        let buffer_slice = staging_buffer.slice(..);
+        let buffer_future = buffer_slice.map_async(wgpu::MapMode::Read);
+
+        //Wait for computation to complete
+        self.device.poll(wgpu::Maintain::Wait);
+
+        //Handle computation result and return
+        match buffer_future.await {
+            Ok(()) => {
+                //Get buffer contents
+                let data = buffer_slice.get_mapped_range();
+                let result: Vec<T> = data.chunks_exact(4).map(|b| *bytemuck::from_bytes::<T>(b)).collect();
+                
+                //Drop mapped view
+                drop(data);
+                //Unmap buffer
+                staging_buffer.unmap();
+
+                Some(result)
+            }
+            Err(e) => {
+                eprintln!("Error: {}", e);
+                None
+            }
+        }
     }
 }
 
