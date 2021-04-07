d921493a441bdab33e39f99befdceabb1ab34bf8
gregoryquick <gregory@quicks.me>
2021-04-07 19:10:26 -0400

    Code reorganization
    -Moved forward pass to separate module in preparation adding a back propagation pipeline
    -Added LeakyRelu activation function to shader

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,36 @@
+mod pipelines;
+
 use rand::prelude::*;
 
 use futures::executor::block_on;
 
 fn main() {
     //Network parameters
-    const DATA_DIM: usize = 16;
-    const OUTPUT_DIM: usize = 2;
+    const DATA_DIM: usize = 65536;
+    const OUTPUT_DIM: usize = 12;
 
     //Make gpu pipelines
     let mut gpu_pipeline = block_on(PipelineManager::new(DATA_DIM, OUTPUT_DIM));
-    let forward_pipeline = gpu_pipeline.new_pipeline::<ForwardPass, f32>();
+    let forward_pipeline = gpu_pipeline.new_pipeline::<pipelines::ForwardPass, f32>();
     
     //Make network weights and a test input
+    use rand::distributions::{Distribution, Uniform};
     let mut rng = rand::thread_rng();
+    let dist = Uniform::new(-1.0,1.0);
     let network_weights = {
         const WEIGHT_SIZE: usize = DATA_DIM * OUTPUT_DIM;
         let mut vector: [f32; WEIGHT_SIZE] = [0f32; WEIGHT_SIZE];
         for num in vector.iter_mut() {
-            *num = rng.gen();
+            *num = rng.sample(dist);
         }
         vector
     };
-    println!("Weights:");
-    println!("{:?}", network_weights);
+    //println!("Weights:");
+    //println!("{:?}", network_weights);
     let input_vector = {
         let mut vector: [f32; DATA_DIM] = [0f32; DATA_DIM];
         for num in vector.iter_mut() {
-            *num = rng.gen();
+            *num = rng.sample(dist);
         }
         vector
     };
@@ -73,11 +77,12 @@ impl PipelineManager{
         }
     }
 
-    fn new_pipeline<P: Pipeline, T: bytemuck::Pod>(&self) -> P {
+    fn new_pipeline<P: pipelines::Pipeline, T: bytemuck::Pod>(&self) -> P {
         P::new::<T>(&self.device, self.network_shape.0, self.network_shape.1)
     }
 
-    async fn run_forward_pass<T: bytemuck::Pod>(&self, pipeline: ForwardPass, network_weights: &[T], input_vector: &[T]) -> Option<Vec<T>> {
+    async fn run_forward_pass<T: bytemuck::Pod>(&self, pipeline: pipelines::ForwardPass, network_weights: &[T], input_vector: &[T]) -> Option<Vec<T>> {
+        use pipelines::*;
         let type_size = std::mem::size_of::<T>() as wgpu::BufferAddress;
 
         //Create command encoder
@@ -177,191 +182,5 @@ impl PipelineManager{
                 None
             }
         }
-    }
-}
-
-trait Pipeline {
-    fn new<T: bytemuck::Pod>(device: &wgpu::Device, input_size: usize, output_size: usize,) -> Self;
-
-    fn get_buffers(&self) -> &Vec<wgpu::Buffer>;
-
-    fn get_bind_group(&self) -> &wgpu::BindGroup;
-
-    fn get_compute_pipeline(&self) -> &wgpu::ComputePipeline;
-}
-
-struct ForwardPass {
-    buffers: Vec<wgpu::Buffer>,
-    bind_group: wgpu::BindGroup,
-    compute_pipeline: wgpu::ComputePipeline,
-}
-
-impl Pipeline for ForwardPass {
-    fn new<T: bytemuck::Pod>(device: &wgpu::Device, input_size: usize, output_size: usize,) -> Self{
-
-        //Create buffers
-        use wgpu::util::{BufferInitDescriptor, DeviceExt};
-        let mut buffers: Vec<wgpu::Buffer> = Vec::new();
-        let type_size = std::mem::size_of::<T>() as wgpu::BufferAddress;
-
-        let uniform_buffer = device.create_buffer_init(
-            &BufferInitDescriptor {
-                label: Some("Uniform Buffer"),
-                contents: bytemuck::bytes_of(&input_size),
-                usage: wgpu::BufferUsage::UNIFORM,
-            }
-        );
-        buffers.push(uniform_buffer);
-
-        let weight_buffer = device.create_buffer(
-            &wgpu::BufferDescriptor {
-                label: Some("Network Weights"),
-                size: (type_size * input_size as u64 * output_size as u64) as wgpu::BufferAddress,
-                usage: wgpu::BufferUsage::STORAGE | wgpu::BufferUsage::COPY_DST,
-                mapped_at_creation: false,
-            }
-        );
-        buffers.push(weight_buffer);
-
-        let input_buffer = device.create_buffer(
-            &wgpu::BufferDescriptor {
-                label: Some("Input Buffer"),
-                size: (type_size * input_size as u64) as wgpu::BufferAddress,
-                usage: wgpu::BufferUsage::STORAGE | wgpu::BufferUsage::COPY_DST,
-                mapped_at_creation: false,
-            }
-        );
-        buffers.push(input_buffer);
-        
-        let output_buffer = device.create_buffer(
-            &wgpu::BufferDescriptor {
-                label: Some("Output buffer"),
-                size: (type_size * output_size as u64) as wgpu::BufferAddress,
-                usage: wgpu::BufferUsage::STORAGE | wgpu::BufferUsage::COPY_SRC,
-                mapped_at_creation: false,
-            }
-        );
-        buffers.push(output_buffer);
-        
-        //Create buffer bind group for pipeline
-        let bind_group_layout = device.create_bind_group_layout(
-            &wgpu::BindGroupLayoutDescriptor {
-                label: None,
-                entries: &[wgpu::BindGroupLayoutEntry {
-                    binding: 0,
-                    visibility: wgpu::ShaderStage::COMPUTE,
-                    ty: wgpu::BindingType::Buffer {
-                        ty: wgpu::BufferBindingType::Uniform,
-                        has_dynamic_offset: false,
-                        min_binding_size: wgpu::BufferSize::new(0),
-                    },
-                    count: None,
-                },
-                wgpu::BindGroupLayoutEntry {
-                    binding: 1,
-                    visibility: wgpu::ShaderStage::COMPUTE,
-                    ty: wgpu::BindingType::Buffer {
-                        ty: wgpu::BufferBindingType::Storage {
-                            read_only: false,
-                        },
-                        has_dynamic_offset: false,
-                        min_binding_size: wgpu::BufferSize::new(0),
-                    },
-                    count: None,
-                },
-                wgpu::BindGroupLayoutEntry {
-                    binding: 2,
-                    visibility: wgpu::ShaderStage::COMPUTE,
-                    ty: wgpu::BindingType::Buffer {
-                        ty: wgpu::BufferBindingType::Storage {
-                            read_only: false,
-                        },
-                        has_dynamic_offset: false,
-                        min_binding_size: wgpu::BufferSize::new(0),
-                    },
-                    count: None,
-                },
-                wgpu::BindGroupLayoutEntry {
-                    binding: 3,
-                    visibility: wgpu::ShaderStage::COMPUTE,
-                    ty: wgpu::BindingType::Buffer {
-                        ty: wgpu::BufferBindingType::Storage {
-                            read_only: false,
-                        },
-                        has_dynamic_offset: false,
-                        min_binding_size: wgpu::BufferSize::new(0),
-                    },
-                    count: None,
-                },],
-            }
-        );
-        let bind_group = device.create_bind_group(
-            &wgpu::BindGroupDescriptor {
-                label: Some("Forward Pass bind group"),
-                layout: &bind_group_layout,
-                entries: &[wgpu::BindGroupEntry {
-                    binding: 0,
-                    resource: buffers[0].as_entire_binding(),
-                },
-                wgpu::BindGroupEntry {
-                    binding: 1,
-                    resource: buffers[1].as_entire_binding(),
-                },
-                wgpu::BindGroupEntry {
-                    binding: 2,
-                    resource: buffers[2].as_entire_binding(),
-                },
-                wgpu::BindGroupEntry {
-                    binding: 3,
-                    resource: buffers[3].as_entire_binding(),
-                },],
-            }
-        );
-
-        //Create compute pipeline
-        let cs_src = include_str!("shaders/forward.comp");
-        let mut compiler = shaderc::Compiler::new().unwrap();
-        let cs_spirv = compiler.compile_into_spirv(cs_src, shaderc::ShaderKind::Compute, "shader.comp", "main", None).unwrap();
-        let cs_module = device.create_shader_module(
-            &wgpu::ShaderModuleDescriptor {
-                label: None,
-                source: wgpu::util::make_spirv(cs_spirv.as_binary_u8()),
-                flags: wgpu::ShaderFlags::empty(),
-            }
-        );
-        
-        let pipeline_layout = device.create_pipeline_layout(
-            &wgpu::PipelineLayoutDescriptor {
-                label: None,
-                bind_group_layouts: &[&bind_group_layout],
-                push_constant_ranges: &[],
-            }
-        );
-        
-        let compute_pipeline = device.create_compute_pipeline(
-            &wgpu::ComputePipelineDescriptor {
-                label: None,
-                layout: Some(&pipeline_layout),
-                module: &cs_module,
-                entry_point: "main",
-            }
-        );
-        ForwardPass{
-            buffers,
-            bind_group,
-            compute_pipeline,
-        }
-    }
-
-    fn get_buffers(&self) -> &Vec<wgpu::Buffer>{
-        &self.buffers
-    }
-
-    fn get_bind_group(&self) -> &wgpu::BindGroup{
-        &self.bind_group
-    }
-
-    fn get_compute_pipeline(&self) -> &wgpu::ComputePipeline{
-        &self.compute_pipeline
     }
 }
---
+++ src/pipelines.rs
@@ -0,0 +1,185 @@
+pub trait Pipeline {
+    fn new<T: bytemuck::Pod>(device: &wgpu::Device, input_size: usize, output_size: usize,) -> Self;
+
+    fn get_buffers(&self) -> &Vec<wgpu::Buffer>;
+
+    fn get_bind_group(&self) -> &wgpu::BindGroup;
+
+    fn get_compute_pipeline(&self) -> &wgpu::ComputePipeline;
+}
+
+pub struct ForwardPass {
+    buffers: Vec<wgpu::Buffer>,
+    bind_group: wgpu::BindGroup,
+    compute_pipeline: wgpu::ComputePipeline,
+}
+
+impl Pipeline for ForwardPass {
+    fn new<T: bytemuck::Pod>(device: &wgpu::Device, input_size: usize, output_size: usize,) -> Self{
+
+        //Create buffers
+        use wgpu::util::{BufferInitDescriptor, DeviceExt};
+        let mut buffers: Vec<wgpu::Buffer> = Vec::new();
+        let type_size = std::mem::size_of::<T>() as wgpu::BufferAddress;
+
+        let uniform_buffer = device.create_buffer_init(
+            &BufferInitDescriptor {
+                label: Some("Uniform Buffer"),
+                contents: bytemuck::bytes_of(&input_size),
+                usage: wgpu::BufferUsage::UNIFORM,
+            }
+        );
+        buffers.push(uniform_buffer);
+
+        let weight_buffer = device.create_buffer(
+            &wgpu::BufferDescriptor {
+                label: Some("Network Weights"),
+                size: (type_size * input_size as u64 * output_size as u64) as wgpu::BufferAddress,
+                usage: wgpu::BufferUsage::STORAGE | wgpu::BufferUsage::COPY_DST,
+                mapped_at_creation: false,
+            }
+        );
+        buffers.push(weight_buffer);
+
+        let input_buffer = device.create_buffer(
+            &wgpu::BufferDescriptor {
+                label: Some("Input Buffer"),
+                size: (type_size * input_size as u64) as wgpu::BufferAddress,
+                usage: wgpu::BufferUsage::STORAGE | wgpu::BufferUsage::COPY_DST,
+                mapped_at_creation: false,
+            }
+        );
+        buffers.push(input_buffer);
+        
+        let output_buffer = device.create_buffer(
+            &wgpu::BufferDescriptor {
+                label: Some("Output buffer"),
+                size: (type_size * output_size as u64) as wgpu::BufferAddress,
+                usage: wgpu::BufferUsage::STORAGE | wgpu::BufferUsage::COPY_SRC,
+                mapped_at_creation: false,
+            }
+        );
+        buffers.push(output_buffer);
+        
+        //Create buffer bind group for pipeline
+        let bind_group_layout = device.create_bind_group_layout(
+            &wgpu::BindGroupLayoutDescriptor {
+                label: None,
+                entries: &[wgpu::BindGroupLayoutEntry {
+                    binding: 0,
+                    visibility: wgpu::ShaderStage::COMPUTE,
+                    ty: wgpu::BindingType::Buffer {
+                        ty: wgpu::BufferBindingType::Uniform,
+                        has_dynamic_offset: false,
+                        min_binding_size: wgpu::BufferSize::new(0),
+                    },
+                    count: None,
+                },
+                wgpu::BindGroupLayoutEntry {
+                    binding: 1,
+                    visibility: wgpu::ShaderStage::COMPUTE,
+                    ty: wgpu::BindingType::Buffer {
+                        ty: wgpu::BufferBindingType::Storage {
+                            read_only: false,
+                        },
+                        has_dynamic_offset: false,
+                        min_binding_size: wgpu::BufferSize::new(0),
+                    },
+                    count: None,
+                },
+                wgpu::BindGroupLayoutEntry {
+                    binding: 2,
+                    visibility: wgpu::ShaderStage::COMPUTE,
+                    ty: wgpu::BindingType::Buffer {
+                        ty: wgpu::BufferBindingType::Storage {
+                            read_only: false,
+                        },
+                        has_dynamic_offset: false,
+                        min_binding_size: wgpu::BufferSize::new(0),
+                    },
+                    count: None,
+                },
+                wgpu::BindGroupLayoutEntry {
+                    binding: 3,
+                    visibility: wgpu::ShaderStage::COMPUTE,
+                    ty: wgpu::BindingType::Buffer {
+                        ty: wgpu::BufferBindingType::Storage {
+                            read_only: false,
+                        },
+                        has_dynamic_offset: false,
+                        min_binding_size: wgpu::BufferSize::new(0),
+                    },
+                    count: None,
+                },],
+            }
+        );
+        let bind_group = device.create_bind_group(
+            &wgpu::BindGroupDescriptor {
+                label: Some("Forward Pass bind group"),
+                layout: &bind_group_layout,
+                entries: &[wgpu::BindGroupEntry {
+                    binding: 0,
+                    resource: buffers[0].as_entire_binding(),
+                },
+                wgpu::BindGroupEntry {
+                    binding: 1,
+                    resource: buffers[1].as_entire_binding(),
+                },
+                wgpu::BindGroupEntry {
+                    binding: 2,
+                    resource: buffers[2].as_entire_binding(),
+                },
+                wgpu::BindGroupEntry {
+                    binding: 3,
+                    resource: buffers[3].as_entire_binding(),
+                },],
+            }
+        );
+
+        //Create compute pipeline
+        let cs_src = include_str!("shaders/forward.comp");
+        let mut compiler = shaderc::Compiler::new().unwrap();
+        let cs_spirv = compiler.compile_into_spirv(cs_src, shaderc::ShaderKind::Compute, "shader.comp", "main", None).unwrap();
+        let cs_module = device.create_shader_module(
+            &wgpu::ShaderModuleDescriptor {
+                label: None,
+                source: wgpu::util::make_spirv(cs_spirv.as_binary_u8()),
+                flags: wgpu::ShaderFlags::empty(),
+            }
+        );
+        
+        let pipeline_layout = device.create_pipeline_layout(
+            &wgpu::PipelineLayoutDescriptor {
+                label: None,
+                bind_group_layouts: &[&bind_group_layout],
+                push_constant_ranges: &[],
+            }
+        );
+        
+        let compute_pipeline = device.create_compute_pipeline(
+            &wgpu::ComputePipelineDescriptor {
+                label: None,
+                layout: Some(&pipeline_layout),
+                module: &cs_module,
+                entry_point: "main",
+            }
+        );
+        ForwardPass{
+            buffers,
+            bind_group,
+            compute_pipeline,
+        }
+    }
+
+    fn get_buffers(&self) -> &Vec<wgpu::Buffer>{
+        &self.buffers
+    }
+
+    fn get_bind_group(&self) -> &wgpu::BindGroup{
+        &self.bind_group
+    }
+
+    fn get_compute_pipeline(&self) -> &wgpu::ComputePipeline{
+        &self.compute_pipeline
+    }
+}
